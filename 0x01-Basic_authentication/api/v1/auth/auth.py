76318f8cc50bdade8172803a7cdcf8552cdadfff
emelda-debug <teacayemmie@gmail.com>
2024-08-06 16:18:25 +0400

    changes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,8 +6,10 @@ from flask import request
 from typing import List, TypeVar
 import fnmatch
 
+
 class Auth():
     """ Authentication Class"""
+
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ method to check if auth is required"""
         if path is None:
@@ -18,17 +20,13 @@ class Auth():
             if fnmatch.fnmatch(path, excluded_path):
                 return False
         return True
-    
+
     def authorization_header(self, request=None) -> str:
         """ method to get authorization header"""
         if request is not None:
             return request.headers.get('Authorization', None)
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """Method to get user from request"""
         return None
-
-        
-
-
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,6 +14,7 @@ from models.user import User
 class BasicAuth(Auth):
     """Basic authentication class
     """
+
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
@@ -30,7 +31,7 @@ class BasicAuth(Auth):
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str,
-            ) -> str:
+    ) -> str:
         """Decodes base64-encoded authorization header """
         if type(base64_authorization_header) == str:
             try:
@@ -45,7 +46,7 @@ class BasicAuth(Auth):
     def extract_user_credentials(
             self,
             decoded_base64_authorization_header: str,
-            ) -> Tuple[str, str]:
+    ) -> Tuple[str, str]:
         """Extracts user credentials from a base64-decoded authorization
         header that uses the Basic authentication flow
         """
